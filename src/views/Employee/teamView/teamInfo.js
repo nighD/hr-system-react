25bddd77445f263790bece94c0c3e76de6f7e701
Night <1340legia@gmail.com>
2019-04-29 23:52:13 +0700

    Create Team View

--- src/views/Employee/teamView/teamInfo.js
+++ src/views/Employee/teamView/teamInfo.js
@@ -5,77 +5,80 @@ class TeamDetail extends Component {
   render() {
     return (
       <div className="animated fadeIn">
-      <Col xs="12" lg="6">
-            <Card>
+      <Row>
+       <Col xs="12" sm="6" md="4">
+            <Card className="border-primary">
               <CardHeader>
-                <i className="fa fa-align-justify"></i> Striped Table
+                Card outline primary
               </CardHeader>
               <CardBody>
-                <Table responsive striped>
-                  <thead>
-                  <tr>
-                    <th>Username</th>
-                    <th>Date registered</th>
-                    <th>Role</th>
-                    <th>Status</th>
-                  </tr>
-                  </thead>
-                  <tbody>
-                  <tr>
-                    <td>Yiorgos Avraamu</td>
-                    <td>2012/01/01</td>
-                    <td>Member</td>
-                    <td>
-                      <Badge color="success">Active</Badge>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Avram Tarasios</td>
-                    <td>2012/02/01</td>
-                    <td>Staff</td>
-                    <td>
-                      <Badge color="danger">Banned</Badge>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Quintin Ed</td>
-                    <td>2012/02/01</td>
-                    <td>Admin</td>
-                    <td>
-                      <Badge color="secondary">Inactive</Badge>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Enéas Kwadwo</td>
-                    <td>2012/03/01</td>
-                    <td>Member</td>
-                    <td>
-                      <Badge color="warning">Pending</Badge>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Agapetus Tadeáš</td>
-                    <td>2012/01/21</td>
-                    <td>Staff</td>
-                    <td>
-                      <Badge color="success">Active</Badge>
-                    </td>
-                  </tr>
-                  </tbody>
-                </Table>
-                <Pagination>
-                  <PaginationItem disabled><PaginationLink previous tag="button">Prev</PaginationLink></PaginationItem>
-                  <PaginationItem active>
-                    <PaginationLink tag="button">1</PaginationLink>
-                  </PaginationItem>
-                  <PaginationItem><PaginationLink tag="button">2</PaginationLink></PaginationItem>
-                  <PaginationItem><PaginationLink tag="button">3</PaginationLink></PaginationItem>
-                  <PaginationItem><PaginationLink tag="button">4</PaginationLink></PaginationItem>
-                  <PaginationItem><PaginationLink next tag="button">Next</PaginationLink></PaginationItem>
-                </Pagination>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
+                laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
+                ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
               </CardBody>
             </Card>
           </Col>
+          <Col xs="12" sm="6" md="4">
+            <Card className="border-secondary">
+              <CardHeader>
+                Card outline secondary
+              </CardHeader>
+              <CardBody>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
+                laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
+                ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+              </CardBody>
+            </Card>
+          </Col>
+          <Col xs="12" sm="6" md="4">
+            <Card className="border-success">
+              <CardHeader>
+                Card outline success
+              </CardHeader>
+              <CardBody>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
+                laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
+                ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+              </CardBody>
+            </Card>
+          </Col>
+          <Col xs="12" sm="6" md="4">
+            <Card className="border-info">
+              <CardHeader>
+                Card outline info
+              </CardHeader>
+              <CardBody>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
+                laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
+                ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+              </CardBody>
+            </Card>
+          </Col>
+          <Col xs="12" sm="6" md="4">
+            <Card className="border-warning">
+              <CardHeader>
+                Card outline warning
+              </CardHeader>
+              <CardBody>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
+                laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
+                ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+              </CardBody>
+            </Card>
+          </Col>
+          <Col xs="12" sm="6" md="4">
+            <Card className="border-danger">
+              <CardHeader>
+                Card outline danger
+              </CardHeader>
+              <CardBody>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
+                laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
+                ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+              </CardBody>
+            </Card>
+          </Col>
+          </Row>
       </div>
 
        );
